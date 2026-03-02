95773e3e37be4a246d188c0a84a1b93c2cf9bbf8
Qeggs <get_boot@outlook.com>
2026-03-02 16:43:43 +0800

    feat(API): 添加资源列表功能
    新增了资源列表相关的API接口，包括 resource_list 和
    resource_list_stream 方法，扩展了API功能模块。

--- nexus/API/_apis/list/__init__.py
+++ nexus/API/_apis/list/__init__.py
@@ -2,8 +2,14 @@ from ._file_list import (
     file_list,
     file_list_stream
 )
+from ._resources_list import (
+    resource_list,
+    resource_list_stream
+)
 
 __all__ = [
     "file_list",
-    "file_list_stream"
+    "file_list_stream",
+    "resource_list",
+    "resource_list_stream"
 ]
